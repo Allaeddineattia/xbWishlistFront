084f406bdc91095c23f168daf1df03151d3f14ba
DorraY <youssefdorra@gmail.com>
2021-09-26 12:05:28 +0100

    added static hamburger menu to navbar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -5,9 +5,23 @@ class NavBar extends Component {
     render(){
       return (
         <nav className="navbar-wrapper navbar">
-          <div className="flex-item"><p>Games</p></div>
-            <div className="flex-item"><p>Wishlist</p></div>
-            <div className="flex-item"><p>Languages</p></div>
+            <div className="flex-item">
+                <button className="navbar-toggler third-button" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent22"
+                        aria-controls="navbarSupportedContent22" aria-expanded="false" aria-label="Toggle navigation">
+                    <div className="animated-icon3">
+                        <span></span>
+                        <span></span>
+                        <span></span></div>
+                </button>
+            </div>
+            <div className="flex-item">
+                <span className="navbar-item">Games</span>
+                <span className="navbar-item">Wishlist</span>
+                <span className="navbar-item">Languages</span>
+            </div>
+
+
         </nav>
       );
     }
